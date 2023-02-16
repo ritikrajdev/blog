191d9398dae3ff8558ac1af16a46b62e2392346e
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-17 02:30:20 +0530

    feat: add navbar

--- src/components/elements/Navbar/index.tsx
+++ src/components/elements/Navbar/index.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import './Navbar.css';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className='body-padding'>
+      {/* Nav Heading */}
+      <div>
+        <h1>The Aritifact</h1>
+        <p>
+          <em>Culture & Art blog</em>
+        </p>
+      </div>
+
+      {/* Nav Links */}
+      <ul>
+        <li>
+          <a href='#'>Blog</a>
+        </li>
+        <li>
+          <a href='#'>About</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
