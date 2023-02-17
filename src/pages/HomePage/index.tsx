a47731c46c54a2752907ae60e8503a81dc991bb7
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-17 12:09:54 +0530

    feat: done homepage with cards

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import Card from '../../components/elements/Card';
+
+import posts from '../../constants/mockData.json';
+
+import './HomePage.css';
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  posts.forEach((post, idx) => {
+    if (!post.image.startsWith('/assets/images/')) {
+      posts[idx].image = '/assets/images/' + post.image;
+    }
+  });
+
+  return (
+    <div className='body-padding home-page'>
+      {posts.map((post, idx) => (
+        <Card key={idx} {...post} />
+      ))}
+    </div>
+  );
 }
