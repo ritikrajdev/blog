9786f7aba55d8895c8f962a2f1f44f57b1e38b43
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-23 05:53:50 +0530

    feat: handle network errors in makeRequest

--- src/utils/makeRequest/index.ts
+++ src/utils/makeRequest/index.ts
@@ -1,14 +1,33 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { NavigateFunction } from 'react-router-dom';
+import {
+  ERROR_PAGE_ROUTE,
+  ERROR_WITH_STATUS_PAGE_ROUTE,
+} from '../../constants/routes';
+
 import { ApiEndpoint } from '../../types/apiEndpoint';
 
 export async function makeRequest(
   apiEndpoint: ApiEndpoint,
   axiosRequestConfig: AxiosRequestConfig = {},
+  navigate?: NavigateFunction,
 ) {
-  const response = await axios({
-    ...axiosRequestConfig,
-    ...apiEndpoint,
-  });
-
-  return response.data;
+  try {
+    const response = await axios({
+      ...axiosRequestConfig,
+      ...apiEndpoint,
+    });
+    return response.data;
+  } catch (err: any) {
+    if (navigate) {
+      const errorStatus = err?.response?.status;
+      navigate(
+        errorStatus
+          ? ERROR_WITH_STATUS_PAGE_ROUTE(errorStatus)
+          : ERROR_PAGE_ROUTE,
+      );
+    } else {
+      throw err;
+    }
+  }
 }
